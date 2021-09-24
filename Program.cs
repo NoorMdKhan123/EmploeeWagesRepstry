f391f23966628801922f680c306317492f2b7a08
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-24 19:35:01 +0530

    calculating employee wages of a day

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        /// <summary>
+        /// Defines the entry point of the application.
+        /// </summary>
+        /// <param name="args">The arguments.</param>  
+          static void Main(string[] args)
+        {
+            
+            //variable constant
+            int employee = 1;
+            int employeeWage = 0;
+            //constant
+            int employee_daily_wage_perhr = 20;
+            int wrking_hr_day = 8;
+
+            if (employee == 1)
+            {
+                employeeWage = employee_daily_wage_perhr * wrking_hr_day;
+                Console.WriteLine("Employee wage of 8 hours is" + employeeWage);
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
