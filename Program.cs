484f3b94b62c6656ac3e9671e47c210b293decdc
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-24 17:51:55 +0530

    Checking Attendance

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace EmployeeAttendance1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int employee_is_present = 1;
+            if (employee_is_present == 1)
+                Console.WriteLine("Employee is Present");
+            else
+            {
+                Console.WriteLine("employee is absent");
+            }
+        }
+    }
+}
