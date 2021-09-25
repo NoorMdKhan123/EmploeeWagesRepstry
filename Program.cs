63e0ce6c19432008f87fa32d1722b13af930650b
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-26 05:11:00 +0530

    Employee Wage

--- Program.cs
+++ Program.cs
@@ -18,7 +18,9 @@ namespace EmployeeWage
             int employee_daily_wage_perhr = 20;
             int wrking_hr_day = 8;
 
-            if (employee == 1)
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == 1)
             {
                 employeeWage = employee_daily_wage_perhr * wrking_hr_day;
                 Console.WriteLine("Employee wage of 8 hours is" + employeeWage);
