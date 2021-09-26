a8bf2d7e66a47205294d0919802b25b90c761ffe
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-26 06:56:43 +0530

    calulating employee wage on condition

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace EmpWageCalOnCondtn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constant
+            const int empFullTime = 1;
+            const int empPartTime = 2;
+            const int empWagePerHour = 20;
+            const int noWorkingDays = 20;
+            const int maxWorkingHours = 100;
+
+
+            //variable
+            int empHr;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            int totalEmpWage;
+
+            while (totalEmpHrs <= maxWorkingHours && totalWorkingDays < noWorkingDays)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+
+                switch (empCheck)
+
+                {
+                    case empFullTime:
+                        empHr = 15;
+                        break;
+
+                    case empPartTime:
+                        empHr = 8;
+                        break;
+
+                    default:
+                        empHr = 0;
+                        break;
+                }
+                totalEmpHrs += empHr;
+                Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + totalEmpHrs);
+            }
+            totalEmpWage = totalEmpHrs * empWagePerHour;
+            Console.WriteLine("Employee wage of a month" + totalEmpWage);
+        }
+    }
+}
+
+
+
+
