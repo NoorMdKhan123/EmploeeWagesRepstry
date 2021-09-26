3da15d3c5705f33df382fc9f77bcd5b924e4b852
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-26 06:11:19 +0530

    Calculated employee wage of part time

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace EmpWagePartTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constant
+            int empFullTime = 1;
+            int empPartTime = 2;
+            int empWagePerHour = 20;
+
+            //variable
+            int empWage = 0;
+            int empHr = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+
+            if(empCheck == empFullTime)
+            {
+                empHr = 15;
+            }
+            else if(empCheck == empPartTime)
+            {
+                empHr = 8;
+            }
+            else
+            {
+                empWage = 0;
+            }
+            empWage = empHr * empWagePerHour;
+            Console.WriteLine("Emploayee wage of a day is : " + empWage);
+        }
+    }
+}
