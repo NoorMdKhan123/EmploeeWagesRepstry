d3aa0f264920fde28296327a75d541611711caea
NoorMdKhan123 <noormd6400@gmail.com>
2021-09-26 06:52:14 +0530

    Calculating employee wages of a month

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace EmpWageofMonth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //constant
+            const int empFullTime = 1;
+            const int empPartTime = 2;
+            const int empWagePerHour = 20;
+            const int totalWorkingDays = 20;
+
+            //variable
+            int empWage = 0;
+            int empHr = 0;
+            int empWageofDay = 0;
+        
+            for (int day = 0; day <= totalWorkingDays; day++)
+            {
+                Random random = new Random();
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+
+                {
+                    case empFullTime:
+                        empHr = 15;
+                        break;
+
+                    case empPartTime:
+                        empHr = 8;
+                        break;
+
+                    default:
+                        empHr = 0;
+                        break;
+                }
+
+
+                empWage = empHr * empWagePerHour;
+                empWageofDay += empWage;
+                Console.WriteLine("Emploayee wage of a day is : " + empWage);
+            }
+       
+            Console.WriteLine("Employee wage of a month" + empWageofDay);
+        }
+    }
+}
